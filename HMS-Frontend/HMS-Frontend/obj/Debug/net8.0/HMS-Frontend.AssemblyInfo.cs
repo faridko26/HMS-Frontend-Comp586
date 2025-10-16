17ed45f5e2ad737e5330c65cb681bfc6b633d398
Farid Koushaneh <faridko@gmail.com>
2025-10-16 16:31:03 -0700

    fix(ui): redirect to sign-in page when clicking Already have an account

--- HMS-Frontend/HMS-Frontend/obj/Debug/net8.0/HMS-Frontend.AssemblyInfo.cs
+++ HMS-Frontend/HMS-Frontend/obj/Debug/net8.0/HMS-Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMS-Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f143e988db36c30d2e974f9402699babadf67c4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+412857688601c4677a1612260f29e361e9865c46")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMS-Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HMS-Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
