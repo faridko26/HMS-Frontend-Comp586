2f0a3969a2a22870a811abf88503a6368260163f
Farid Koushaneh <faridko@gmail.com>
2025-10-31 14:34:06 -0700

    add(ui): add add patient section at the bottom of the dashboard

--- HMS-Frontend/HMS-Frontend/obj/Debug/net8.0/HMS-Frontend.AssemblyInfo.cs
+++ HMS-Frontend/HMS-Frontend/obj/Debug/net8.0/HMS-Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMS-Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5591191edd1655c03160bcdba8f3a1ac3c1abb89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8dccef18817dbc6d9c54803f96c1d1e8bd979b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMS-Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HMS-Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
