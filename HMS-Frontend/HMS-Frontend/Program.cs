0d4008b5888ac0c295a19aa35e52381f124fbfe8
Farid Koushaneh <faridko@gmail.com>
2025-12-11 19:22:50 -0800

    update program.cs and fix some errors

--- HMS-Frontend/HMS-Frontend/Program.cs
+++ HMS-Frontend/HMS-Frontend/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Options;
+using HMS_Frontend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,29 +16,30 @@ builder.Services.AddSession(options =>
 });
 
 // ===== Auth client (you already had this) =====
-var useFake = builder.Configuration.GetValue<bool>("Frontend" +
-    "" +
-    ":UseFakeApi");
-if (useFake)
-{
-    builder.Services.AddSingleton<IAuthApiClient, FakeAuthApiClient>();
-}
-else
-{
-    builder.Services.AddHttpClient<IAuthApiClient, AuthApiClient>((sp, http) =>
-    {
-        var opts = sp.GetRequiredService<IOptions<BackendOptions>>().Value;
-        if (string.IsNullOrWhiteSpace(opts.BaseUrl))
-            throw new InvalidOperationException("Backend:BaseUrl is not configured.");
-        http.BaseAddress = new Uri(opts.BaseUrl!.TrimEnd('/') + "/");
-    });
-}
+// ===== Factory Registration =====
+builder.Services.AddSingleton<HMS_Frontend.Services.Factories.IApiClientFactory, HMS_Frontend.Services.Factories.ApiClientFactory>();
 
-// ===== Users client (ADD THIS) =====
-// Handler that attaches "Authorization: Bearer <token>" from session
+// ===== Auth client =====
+// Register both implementations, Factory will decide which one to return if used manually, 
+// or we can use the factory to register the interface.
+builder.Services.AddSingleton<FakeAuthApiClient>();
+builder.Services.AddHttpClient<AuthApiClient>((sp, http) =>
+{
+    var opts = sp.GetRequiredService<IOptions<BackendOptions>>().Value;
+    if (string.IsNullOrWhiteSpace(opts.BaseUrl))
+        throw new InvalidOperationException("Backend:BaseUrl is not configured.");
+    http.BaseAddress = new Uri(opts.BaseUrl!.TrimEnd('/') + "/");
+});
+
+// Register IAuthApiClient using the Factory
+builder.Services.AddTransient<IAuthApiClient>(sp =>
+    sp.GetRequiredService<HMS_Frontend.Services.Factories.IApiClientFactory>().CreateAuthClient());
+
+
+// ===== Users client =====
 builder.Services.AddTransient<JwtSessionHandler>();
 
-builder.Services.AddHttpClient<IUsersApiClient, UsersApiClient>((sp, http) =>
+builder.Services.AddHttpClient<UsersApiClient>((sp, http) =>
 {
     var opts = sp.GetRequiredService<IOptions<BackendOptions>>().Value;
     if (string.IsNullOrWhiteSpace(opts.BaseUrl))
@@ -46,14 +48,48 @@ builder.Services.AddHttpClient<IUsersApiClient, UsersApiClient>((sp, http) =>
 })
 .AddHttpMessageHandler<JwtSessionHandler>();
 
+builder.Services.AddTransient<IUsersApiClient>(sp =>
+    sp.GetRequiredService<HMS_Frontend.Services.Factories.IApiClientFactory>().CreateUsersClient());
+
+
+// ===== General API Service =====
+builder.Services.AddHttpClient<ApiService>((sp, http) =>
+{
+    var opts = sp.GetRequiredService<IOptions<BackendOptions>>().Value;
+    if (string.IsNullOrWhiteSpace(opts.BaseUrl))
+        throw new InvalidOperationException("Backend:BaseUrl is not configured.");
+    http.BaseAddress = new Uri(opts.BaseUrl!.TrimEnd('/') + "/");
+})
+.AddHttpMessageHandler<JwtSessionHandler>();
+
+builder.Services.AddTransient<IApiService>(sp =>
+    sp.GetRequiredService<HMS_Frontend.Services.Factories.IApiClientFactory>().CreateApiService());
+
+// Authentication
+builder.Services.AddAuthentication(Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/SignIn";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    });
+
 // Pages
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseSession();
 
 app.MapRazorPages();
