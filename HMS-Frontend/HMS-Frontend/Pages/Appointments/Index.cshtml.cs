b3d7b0fc3b430160549b8026af594c931107bc4e
Farid Koushaneh <faridko@gmail.com>
2025-12-11 18:56:12 -0800

    add appointment page

---
+++ HMS-Frontend/HMS-Frontend/Pages/Appointments/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HMS_Frontend.Pages.Appointments
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- HMS-Frontend/HMS-Frontend/obj/Debug/net8.0/HMS-Frontend.AssemblyInfo.cs
+++ HMS-Frontend/HMS-Frontend/obj/Debug/net8.0/HMS-Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMS-Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca7edd2c30e3af50a2aefadc923228a40313c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d507063a6a62f99e0bac9977bb6443757b812e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMS-Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HMS-Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
